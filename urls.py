426aedf2fcee1f0a78e42beb8ae14f52a541219f
manjusree-kc <115994703+manjusree-kc@users.noreply.github.com>
2024-09-23 16:35:44 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,17 +1,12 @@
-from django.contrib import admin
 from django.urls import path
-from reg_app.views import *
+from .views import *
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("statistics-view/", statistics_view ),
-    path("regulations-list/", regulations_list ),
-    path("get_regulation_details/", get_regulation_details, name="get_regulation_details"),
+    path('get_regulation/<int:not_id>/', get_regulation_details, name='get_regulation_details'),
+    path('get_table_data/', get_table_data, name='get_table_data'),
+    path('statistics_view/', statistics_view, name='statistics_view'),
     path("get_impact_level_pie_data/", get_impact_level_pie_data, name="get_impact_level_pie_data"),
     path("get_notifications_count_per_body/", get_notifications_count_per_body, name="get_notifications_count_per_body"),
     path("get_quarterly_data/", get_quarterly_data, name="get_quarterly_data"),
-
-
-
-    
+    path("calculate_response_time_intervals/", calculate_response_time_intervals, name="calculate_response_time_intervals")
 ]
